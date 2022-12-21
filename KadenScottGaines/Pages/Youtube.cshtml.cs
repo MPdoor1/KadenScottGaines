836455a600589f0f8d8f6019e4f2f9e1c05602b6
MPdoor1 <107142603+MPdoor1@users.noreply.github.com>
2022-12-21 09:59:49 -0500

    Updating stuff

---
+++ KadenScottGaines/Pages/Youtube.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KadenScottGaines.Pages
+{
+    public class YoutubeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
